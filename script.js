efc5971e41204773694bc3c21cd49631239c2b73
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-09-11 12:34:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-// find Current date
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// Find current date
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDate = new Date();
 let day = weekday[currentDate.getDay()];
 document.getElementById("CurrentDay").innerHTML = day;
 
-
-// find Current Time
+// Find current time
 const CurrentUTCtime = new Date(currentDate);
-let time = CurrentUTCtime.getTime();
-let hours=CurrentUTCtime.getUTCHours();
-const minutes=CurrentUTCtime.getUTCMinutes();
-const Currenttime=`${hours+2}:${minutes<10?"0" : ""}${minutes}`;
-
+let hours = CurrentUTCtime.getUTCHours();
+const minutes = CurrentUTCtime.getUTCMinutes();
+const Currenttime =   ${hours + 2}:${minutes < 10 ? "0" : ""}${minutes}  ;
 document.getElementById("CurrentUTCtime").innerHTML = Currenttime;
+
+// Include milliseconds
+const milliseconds = CurrentUTCtime.getUTCMilliseconds();
+document.getElementById("Milliseconds").innerHTML = milliseconds;
+
+// Include track
+const track = "Frontend"; // Replace with your desired track
+document.getElementById("myTrack").innerHTML = track;
